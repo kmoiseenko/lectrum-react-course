10ef8e01f7f3bd925a52748354cb3055a7bb6521
Konstantin Moiseenko <konstantin.moiseenko@ab-soft.net>
2019-10-21 18:00:28 +0300

    Lesson 2, task 3

--- src/Components/Accordion/index.js
+++ src/Components/Accordion/index.js
@@ -1,23 +1,34 @@
-import React from 'react';
-// import { string, number, array } from 'prop-types';
+import React, { useState } from 'react';
+import cx from 'classnames';
+import { string, number } from 'prop-types';
 
 // Styles
 import './styles.scss';
 
-export const Accordion = (source) => {
+export const Accordion = (props) => {
+  const { question, answer } = props.source
+  const [index] = useState(props.index);
+  const [activeClassName, updateActiveClassName] = useState('');
+
+  const customClassNames = cx({
+    active: activeClassName,
+    'app-accordion__head': 'app-accordion__head'
+  });
+
+  const handleClick = () => {
+    updateActiveClassName(activeClassName === '' ? 'active' : '');
+  }
+
   return (
       <div className="app-accordion">
-        
+        <div className={ customClassNames } onClick={ handleClick }>{ index + 1 }.{ question }</div>
+        <div className="app-accordion__body">{ answer }</div>
       </div>
   );
 };
 
-// Accordion.propTypes = {
-//   title: string.isRequired,
-//   description: string.isRequired,
-//   published: string.isRequired,
-//   likes: number.isRequired,
-//   comments: number.isRequired,
-//   // image: 
-//   tags: array.isRequired
-// };
+Accordion.propTypes = {
+  question: string.isRequired,
+  answer: string.isRequired,
+  index: number.isRequired
+};
--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,17 +1,27 @@
 import React from 'react';
 
 // Components
-import { News } from '../Components/News';
+import { Accordion } from '../Components/Accordion';
 
 // Styles
 import './styles.scss';
 
+// Source
+import source from './accordion-source.json';
+
 export const Home = () => {
+  const accordionsJSX = source.map((item, index) => {
+    return <Accordion key={ index } source={ item } index={ index } />
+  });
+
   return (
     <>
       <div className="app">
         <div className="app-container">
-          <News />
+          <h1>Project brief</h1>
+          <div className="app-accordion__list">
+            { accordionsJSX }
+          </div>
         </div>
       </div>
     </>
