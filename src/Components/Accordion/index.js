06e3e39e163453d3f673a834911dd23bd9cfa3cd
Konstantin Moiseenko <konstantin.moiseenko@ab-soft.net>
2019-10-21 15:16:53 +0300

    Added Accordion component

---
+++ src/Components/Accordion/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+// import { string, number, array } from 'prop-types';
+
+// Styles
+import './styles.scss';
+
+export const Accordion = (source) => {
+  return (
+      <div className="app-accordion">
+        
+      </div>
+  );
+};
+
+// Accordion.propTypes = {
+//   title: string.isRequired,
+//   description: string.isRequired,
+//   published: string.isRequired,
+//   likes: number.isRequired,
+//   comments: number.isRequired,
+//   // image: 
+//   tags: array.isRequired
+// };
