60c2bc1aacc15356b1e809a998b26214afe24742
Konstantin Moiseenko <konstantin.moiseenko@ab-soft.net>
2019-10-23 17:30:21 +0300

    Some changes in structer. Advise from Andrey

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -6,12 +6,15 @@ import { News } from '../Components/News';
 // Styles
 import './styles.scss';
 
+// Source
+import source from './source.json';
+
 export const Home = () => {
   return (
     <>
       <div className="app">
         <div className="app-container">
-          <News />
+          <News source={ source } />
         </div>
       </div>
     </>
