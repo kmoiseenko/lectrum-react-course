0721d9f9bf6983b2fe995c214766904b998b5bd4
Konstantin Moiseenko <konstantin.moiseenko@ab-soft.net>
2019-11-03 14:45:20 +0200

    Lesson 5 task 4

--- src/Components/StudentRegistration/index.js
+++ src/Components/StudentRegistration/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 // Navifation
 import { history } from './../../navigation/history';
 import { book } from './../../navigation/book';
+
+// Styles
+import './styles.scss';
 
 export const StudentRegistration = () => {
     const getInitValues = () => {
@@ -37,12 +40,37 @@ export const StudentRegistration = () => {
 
     const initialValues = getInitValues();
     const textValues = getTextValues();
+    const formErrorMessages = {
+        required: 'This field is required',
+        age: 'Age must be more then 6 and less then 60',
+        email: 'This is not a valid email'
+    }
 
     const handleFormSubmit = (values) => {
         console.log(values);
         localStorage.setItem('student', JSON.stringify(values));
         alert(textValues.successMessage);
         history.replace(book.student);
+    }
+
+    const validateSimpleFields = (value) => {
+        return !value ? formErrorMessages.required : null;
+    }
+
+    const validateAgeField = (value) => {
+        if(value) {
+            return value > 6 && value < 60 ? null : formErrorMessages.age;
+        }
+    }
+
+    const validateEmailField = (value) => {
+        let re = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+
+        if(value) {
+            return re.test(value) ? null : formErrorMessages.email;
+        } else {
+            return formErrorMessages.required;
+        }
     }
 
     return (
@@ -53,77 +81,135 @@ export const StudentRegistration = () => {
                 initialValues={ initialValues }
                 onSubmit={ handleFormSubmit }
             >
-                <Form>
-                    <div>
-                        <label htmlFor="firstName">First name</label>
-                        <Field
-                            type="text"
-                            id="firstName"
-                            name="firstName"
-                            placeholder="Enter your first name"
-                        />
-                    </div>
-                    
-                    <div>
-                        <label htmlFor="surname">Surname</label>
-                        <Field
-                            type="text"
-                            id="surname"
-                            name="surname"
-                            placeholder="Enter your surname"
-                        />
-                    </div>
+                {(props) => {
+                    const { errors, touched } = props;
+                    return (
+                        <Form>
+                            <div>
+                                <label
+                                    className={ touched.firstName && errors.firstName ? 'error' : null }
+                                    htmlFor="firstName"
+                                >
+                                    First name
+                                </label>
+                                <Field
+                                    type="text"
+                                    id="firstName"
+                                    className={ touched.firstName && errors.firstName ? 'error' : null }
+                                    name="firstName"
+                                    placeholder="Enter your first name"
+                                    validate={ validateSimpleFields }
+                                />
+                                <ErrorMessage name="firstName" />
+                            </div>
+                            
+                            <div>
+                                <label 
+                                    className={ touched.firstName && errors.firstName ? 'error' : null }
+                                    htmlFor="surname"
+                                >
+                                    Surname
+                                </label>
+                                <Field
+                                    type="text"
+                                    id="surname"
+                                    className={ touched.firstName && errors.firstName ? 'error' : null }
+                                    name="surname"
+                                    placeholder="Enter your surname"
+                                    validate={ validateSimpleFields }
+                                />
+                                <ErrorMessage name="surname" />
+                            </div>
 
-                    <div>
-                        <label htmlFor="age">Age</label>
-                        <Field
-                            type="number"
-                            id="age"
-                            name="age"
-                            placeholder="Enter your age"
-                        />
-                    </div>
+                            <div>
+                                <label
+                                    className={ touched.firstName && errors.firstName ? 'error' : null }
+                                    htmlFor="age"
+                                >
+                                    Age
+                                </label>
+                                <Field
+                                    type="number"
+                                    id="age"
+                                    className={ touched.firstName && errors.firstName ? 'error' : null }
+                                    name="age"
+                                    placeholder="Enter your age"
+                                    validate={ validateAgeField }
+                                />
+                                <ErrorMessage name="age" />
+                            </div>
 
-                    <div>
-                        <label htmlFor="email">Email</label>
-                        <Field
-                            type="email"
-                            id="email"
-                            name="email"
-                            placeholder="Enter your Email"
-                        />
-                    </div>
+                            <div>
+                                <label
+                                    className={ touched.firstName && errors.firstName ? 'error' : null }
+                                    htmlFor="email"
+                                >
+                                    Email
+                                </label>
+                                <Field
+                                    type="email"
+                                    id="email"
+                                    className={ touched.firstName && errors.firstName ? 'error' : null }
+                                    name="email"
+                                    placeholder="Enter your Email"
+                                    validate={ validateEmailField }
+                                />
+                                <ErrorMessage name="email" />
+                            </div>
 
-                    <div>
-                        <label>Choose your sex</label>
-                        <label htmlFor="male">Male</label>
-                        <Field
-                            type="radio"
-                            id="male"
-                            name="sex"
-                            value="Male"
-                        />
-                        <label htmlFor="female">Female</label>
-                        <Field
-                            type="radio"
-                            id="female"
-                            name="sex"
-                            value="Female"
-                        />
-                    </div>
+                            <div>
+                                <label
+                                    className={ touched.firstName && errors.firstName ? 'error' : null }
+                                >
+                                    Choose your sex
+                                </label>
+                                <label htmlFor="male">Male</label>
+                                <Field
+                                    type="radio"
+                                    id="male"
+                                    className={ touched.firstName && errors.firstName ? 'error' : null }
+                                    name="sex"
+                                    value="Male"
+                                    validate={ validateSimpleFields }
+                                />
+                                <label htmlFor="female">Female</label>
+                                <Field
+                                    type="radio"
+                                    id="female"
+                                    className={ touched.firstName && errors.firstName ? 'error' : null }
+                                    name="sex"
+                                    value="Female"
+                                    validate={ validateSimpleFields }
+                                />
+                                <ErrorMessage name="sex" />
+                            </div>
 
-                    <div>
-                        <label htmlFor="speciality">Choose your speciality</label>
-                        <Field as="select" name="speciality" id="speciality">
-                            <option disabled>Choose your speciality</option>
-                            <option value="designer">Designer</option>
-                            <option value="developer">Developer</option>
-                            <option value="writer">Writer</option>
-                        </Field>
-                    </div>
+                            <div>
+                                <label
+                                    className={ touched.firstName && errors.firstName ? 'error' : null }
+                                    htmlFor="speciality"
+                                >
+                                    Choose your speciality
+                                </label>
+                                <Field
+                                    as="select"
+                                    name="speciality"
+                                    id="speciality"
+                                    className={ touched.firstName && errors.firstName ? 'error' : null }
+                                    validate={ validateSimpleFields }
+                                >
+                                    <option value="">Choose your speciality</option>
+                                    <option value="designer">Designer</option>
+                                    <option value="developer">Developer</option>
+                                    <option value="writer">Writer</option>
+                                </Field>
+                                <ErrorMessage name="speciality" />
+                            </div>
 
-                    <button className="app-btn" type="submit">{ textValues.btn }</button>
-                </Form>
+                            <button className="app-btn" type="submit">{ textValues.btn }</button>
+                        </Form>
+                    )
+                }}
             </Formik>
         </section>
     )
